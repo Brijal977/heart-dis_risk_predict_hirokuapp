ed71dac3ea5452c2496ae034f4aa1ca720da2fc9
basudev rijal <mail.basu12@gmail.com>
2023-03-31 13:42:33 -0400

    updated to latest python libraries

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from prediction import show_predict_page
 from explore import show_explore_page
-from PIL import Image
+#from pillow import Image
 
 st.set_page_config(
     page_title="Predict Heart Disease",
